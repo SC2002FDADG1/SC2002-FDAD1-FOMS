80d28a21113e80db163def0e752009fe7bba08df
tweiyew <155275815+tweiyew@users.noreply.github.com>
2024-04-12 17:09:03 +0800

    Add files via upload

---
+++ Food Systems/Menu.java
@@ -0,0 +1,11 @@
+package FoodSystem;
+import java.util.ArrayList;
+
+public class Menu {
+    private ArrayList<Food> foodList; 
+
+    public void displayMenu(Food[] food) {
+        // Implementation code here
+    }
+
+}
