06e45d6395ef16cd4e5b24f841379701874f59dc
DungGojo <159887901+DungGojo@users.noreply.github.com>
2024-04-14 16:41:00 +0800

    DisplayPayment.java

--- PaymentSystem/DisplayPayment.java
+++ PaymentSystem/DisplayPayment.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.paymentsystem;
 
 import com.mycompany.paymentsystem.PaymentType.Payment_Availability;
 import java.util.ArrayList;
 
-/**
- *
- * @author dunglai
- */
 public class DisplayPayment 
 {
     public void displayPayments( ArrayList<PaymentType> array  )
