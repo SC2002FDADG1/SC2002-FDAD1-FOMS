4461712a1c0e264ee52488839cb7a5fdf474707d
tweiyew <155275815+tweiyew@users.noreply.github.com>
2024-04-25 10:23:53 +0800

    Add files via upload

---
+++ OrderSystem/KindOfPickUp.java
@@ -0,0 +1,6 @@
+package OrderSystem;
+
+public enum KindOfPickUp {
+    TABLE_SERVICE,
+    SELF_PICK
+}
---
+++ OrderSystem/Status.java
@@ -0,0 +1,8 @@
+package OrderSystem;
+
+public enum Status {
+    PREPARING,
+    READY_TO_PICK_UP,
+    COMPLETED
+}
+
